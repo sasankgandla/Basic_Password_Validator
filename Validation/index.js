290820bd130649ee9d806beb035b860cc6dc35aa
Gandla sasank <100485669+sasankgandla@users.noreply.github.com>
2023-07-29 14:50:06 +0530

    Add files via upload

---
+++ Validation/index.js
@@ -0,0 +1,32 @@
+const passwordInput = document.querySelector(".pass-field input")
+const eyeIcon = document.querySelector(".pass-field i")
+const requirementList = document.querySelectorAll(".requirement-list li")
+
+
+const requirements = [
+    {regex:/.{8,}/,index:0},
+    {regex:/[0-9]/,index:1},
+    {regex:/[a-z]/,index:2},
+    {regex:/[^A-Za-z0-9]/,index:3},
+    {regex:/[A-Z]/,index:4},
+]
+
+passwordInput.addEventListener("keyup",function(e) {
+    requirements.forEach(item=>{
+        const isValid = item.regex.test(e.target.value);
+        const requirementItem = requirementList[item.index];
+        
+        if(isValid) {
+            requirementItem.firstElementChild.className = "fa-solid fa-check";
+        }
+        else {
+            requirementItem.firstElementChild.className = "fa-solid fa-circle";
+        }
+    })
+})
+
+
+eyeIcon.addEventListener("click",function() {
+    passwordInput.type = passwordInput.type === "password" ? "text" : "password";
+    eyeIcon.className = eyeIcon.className === "fa-solid fa-eye-slash" ? "fa-solid fa-eye" : "fa-solid fa-eye-slash";
+})
